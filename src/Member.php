1b053915aa5a6f308e65795528e74f6714b3b6f9
Mr.justary <justary@126.com>
2021-02-05 13:09:35 +0800

    add file

---
+++ src/Member.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Justary\Flc;
+
+class Member{
+
+	public function desc(){
+		return 'member desc';
+	}
+}
---
+++ src/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Justary\Flc;
+
+class User{
+
+	public function desc(){
+		return 'user desc';
+	}
+}
